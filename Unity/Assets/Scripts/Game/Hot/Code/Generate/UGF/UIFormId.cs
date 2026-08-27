b77046c8aabea804dbd41bd7e962425246d42fa0
PoSuanSama <2913939161@qq.com>
2026-08-27 15:23:09 +0800

    build(luban): 接入多路径校验并刷新 UI 配置与生成产物

--- Unity/Assets/Scripts/Game/Hot/Code/Generate/UGF/UIFormId.cs
+++ Unity/Assets/Scripts/Game/Hot/Code/Generate/UGF/UIFormId.cs
@@ -63,5 +63,20 @@ namespace Game.Hot
         /// FairyGUI演示
         /// </summary>
         public const int FairyDemoForm = 103;
+
+        /// <summary>
+        /// FairyGUI背包界面
+        /// </summary>
+        public const int FairyInventoryForm = 104;
+
+        /// <summary>
+        /// FairyGUI物品详情浮动窗口
+        /// </summary>
+        public const int FairyItemDetailForm = 105;
+
+        /// <summary>
+        /// FairyGUI背包遮罩界面
+        /// </summary>
+        public const int FairyInventoryOverlayForm = 106;
     }
 }
